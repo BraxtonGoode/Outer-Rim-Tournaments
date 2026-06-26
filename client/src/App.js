7baf40173c850213d1f5aae21941de266695e2e6
BraxtonGoode <tjbgoode@gmail.com>
2026-06-25 18:26:03 -0700

    basic start

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Outer Rim Tournaments</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ server/src/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+
+// Routes
+app.get('/api', (req, res) => {
+    res.json({ message: 'Welcome to the API' });
+});
+
+module.exports = app;
